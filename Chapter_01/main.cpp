3a32c5d2892d87442172f60b7631162ff4255fef
EGOH <EGHO@EGHO.com>
2019-10-09 20:24:58 +0900

    Add test code to main.cpp

--- Chapter_01/main.cpp
+++ Chapter_01/main.cpp
@@ -5,6 +5,10 @@ int main()
 {
   Customer customer("Armageddon");
 
+  Movie movie("The day after", Movie::NEW_RELEASE);
+  Rental rental(movie, 5);
+  customer.addRental(rental);
+
   std::cout << customer.statement();
   std::cout.flush();
 
